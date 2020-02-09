feae6fbcce2d2ede480932a7f25f7f2a6ece8898
Lucianodasilvameyer <murcilhax@hotmail.com>
2020-02-09 17:56:21 -0200

    na programação do GameOver

--- Assets/scripts/teste.cs
+++ Assets/scripts/teste.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class teste1 : MonoBehaviour
+public class teste : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
